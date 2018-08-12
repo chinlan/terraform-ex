ffb13635a315d56f1a38803a38b11c737c96a515
ChinLan <nilsoul@hotmail.com.tw>
2018-08-12 18:11:47 +0900

    deploy a cluster of web servers

--- main.tf
+++ main.tf
@@ -11,17 +11,17 @@ output "public_ip" {
   value = "${aws_instance.example.public_ip}"
 }
 
-resource "aws_instance" "example" {
+resource "aws_launch_configuration" "example" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  security_groups = ["${aws_security_group.instance.id}"]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -33,5 +33,22 @@ resource "aws_security_group" "instance" {
     to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
